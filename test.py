e7696c8961a56bfd5b0be5a9ca340e61d8d41ab7
Adrian <adiadrian9798@gmail.com>
2024-07-02 05:03:08 +0300

    3

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-test
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO('yolov8m.pt')
+
+    model.train(data="C:/Users/adiad/OneDrive/Desktop/practica 2024/training/tennis-ball-detection-6/data.yaml", epochs=2, imgsz=640, device='cuda', batch = 10, workers = 16)
+
+    #freeze_support()
